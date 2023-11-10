e2e333906ba8aaac8e8544527e2a693cf187690d
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-11-10 14:46:49 -0300

    Implement tab header content and font icons

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
 import { TabHeaderComponent } from './components/tab-header/tab-header.component';
 
@@ -7,10 +9,12 @@ import { TabHeaderComponent } from './components/tab-header/tab-header.component
   declarations: [],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     TabHeaderComponent,
     TabMenuComponent,
   ],
   exports: [
+    FontAwesomeModule,
     TabHeaderComponent,
     TabMenuComponent,
   ]
--- src/app/base/components/tab-header/tab-header.component.ts
+++ src/app/base/components/tab-header/tab-header.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronLeft,faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tab-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './tab-header.component.html',
   styleUrl: './tab-header.component.scss'
 })
 export class TabHeaderComponent {
 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faChevronLeft,
+      faEllipsisV
+    )
+  }
 }
