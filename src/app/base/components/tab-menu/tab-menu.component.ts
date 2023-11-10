50b9ac3032f620407b1f6cda86cb80d9d3b201b5
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-11-10 15:17:02 -0300

    Implement tab menu content

--- src/app/base/components/tab-menu/tab-menu.component.ts
+++ src/app/base/components/tab-menu/tab-menu.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBox, faDollarSign, faEllipsisH, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faUser } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-tab-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './tab-menu.component.html',
   styleUrl: './tab-menu.component.scss'
 })
 export class TabMenuComponent {
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faBox,
+      faDollarSign,
+      faPenToSquare,
+      faUser,
+      faUsers,
+      faEllipsisH
+    );
+  }
 
 }
