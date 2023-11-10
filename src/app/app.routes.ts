046bd6d1a6cc80f2bc5e00c20a54a3f2ce084bc2
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-11-10 15:41:10 -0300

    Implement default routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { DefaultRouteComponent } from './base/default-route/default-route.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'catalog',
+        component: DefaultRouteComponent
+    },
+    {
+        path: 'proposals',
+        component: DefaultRouteComponent
+    },
+    {
+        path: 'people',
+        component: DefaultRouteComponent
+    },
+    {
+        path: 'payments',
+        component: DefaultRouteComponent
+    },
+    {
+        path: 'more',
+        component: DefaultRouteComponent
+    },
+
+];
---
+++ src/app/base/default-route/default-route.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-default-route',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './default-route.component.html',
+  styleUrl: './default-route.component.scss'
+})
+export class DefaultRouteComponent {
+
+}
